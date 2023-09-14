c986e9c29c32d66e8334e3126cc3e8c54a2f7cb9
Manson-chen <948936249@qq.com>
2023-09-14 16:44:52 +0800

    update readme

--- doj-frontend/src/views/user/UserLoginView.vue
+++ doj-frontend/src/views/user/UserLoginView.vue
@@ -11,12 +11,15 @@
       @submit="handleSubmit"
     >
       <a-form-item field="userAccount" label="账号">
-        <a-input v-model="form.userAccount" placeholder="请输入账号" />
+        <a-input
+          v-model="form.userAccount"
+          placeholder="请输入账号：jiandong"
+        />
       </a-form-item>
       <a-form-item field="userPassword" tooltip="密码不少于 8 位" label="密码">
         <a-input-password
           v-model="form.userPassword"
-          placeholder="请输入密码"
+          placeholder="请输入密码：abc123456"
         />
       </a-form-item>
       <a-form-item>
